9a05d24ba2749dee015d47c5b2c6a87de77a2c2b
Isaac Pavon <isaac_pavon@yahoo.com>
2018-12-02 12:47:14 -0800

    Actually adding a 404 page, plus a fully styled standings page

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.get('/', function (req, res, next) {
     }
   });
 
-app.get('/teams');
+app.get('/teams', function (req, res, next) {
+    res.status(200).render('teams', allTeams["divisions"]);
+});
 
 app.get('/teams/:division/:team');
 
