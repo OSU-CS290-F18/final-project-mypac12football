f120257729baf142c2f8491a5f06b37a976ef2a2
Isaac Pavon <isaac_pavon@yahoo.com>
2018-12-02 11:28:31 -0800

    All Home page functionality done!

--- public/index.js
+++ public/index.js
@@ -1,19 +1,32 @@
 var addTeamButton = document.getElementById('add-teams-button');
 var hideModalButton = document.getElementById('modal-cancel');
 var xModal = document.getElementById('modal-close');
-var postButton = document.getElementById('modal-accept');
+var updateButton = document.getElementById('modal-accept');
 
 addTeamButton.addEventListener("click", function() {
     var modalBackdrop = document.getElementById('modal-backdrop');
     var modal = document.getElementById('add-teams-modal');
     var checkboxes = document.getElementsByName('input-team');
+    var myTeamsArr = document.getElementsByClassName('my-team');
+    var i = 0;
     modalBackdrop.classList.remove('hidden');
     modal.classList.remove('hidden');
+    for(i = 0; i < myTeamsArr.length; i++){
+        checkboxes.forEach(element => {
+            if(element.value === myTeamsArr[i].getAttribute('data-name')){
+                element.checked = true;
+            }
+        });
+    }
 });
 
 hideModalButton.addEventListener("click", function() {
     var modalBackdrop = document.getElementById('modal-backdrop');
     var modal = document.getElementById('add-teams-modal');
+    var checkboxes = document.getElementsByName('input-team');
+    checkboxes.forEach(element => {
+        element.checked = false;
+    });
     modalBackdrop.classList.add('hidden');
     modal.classList.add('hidden');
 });
@@ -21,6 +34,28 @@ hideModalButton.addEventListener("click", function() {
 xModal.addEventListener("click", function() {
     var modalBackdrop = document.getElementById('modal-backdrop');
     var modal = document.getElementById('add-teams-modal');
+    var checkboxes = document.getElementsByName('input-team');
+    checkboxes.forEach(element => {
+        element.checked = false;
+    });
     modalBackdrop.classList.add('hidden');
     modal.classList.add('hidden');
+});
+
+updateButton.addEventListener("click", function() {
+    var myTeams = [];
+    var checkboxes = document.getElementsByName('input-team');
+    checkboxes.forEach(element => {
+        if(element.checked == true){
+            myTeams.push(element.value);
+        }
+    });
+    var pushRequest = new XMLHttpRequest();
+    pushRequest.open('POST', '/updateMyTeams');
+    pushRequest.setRequestHeader('Content-Type', 'application/json');
+
+    pushRequest.send(JSON.stringify(myTeams));
+
+    location.reload(true);
+
 });
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var myTeams = require('./myTeams.json');
+var allTeams = require('./allTeams.json');
 
 var app = express();
 
@@ -9,5 +13,87 @@ var port = process.env.PORT || 8000;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
 
+app.get('/', function (req, res, next) {
+    if(myTeams["myTeams"]["teams"].length == 0){
+        res.status(200).render('initialNoTeams');
+    }
+    else{
+        res.status(200).render('initial', myTeams["myTeams"]);
+    }
+  });
+
+app.post('/updateMyTeams', function (req, res, next) {
+    if(req.body)
+    newMyTeams = req.body;
+    var i = 0;
+    if (myTeams["myTeams"]["teams"].length > 0){
+        if(newMyTeams.length == 0) myTeams["myTeams"]["teams"].length = 0;
+        else{
+    while(i < myTeams["myTeams"]["teams"].length){
+        var j = 0;
+        for(j = 0; j < newMyTeams.length; j++){
+            if(myTeams["myTeams"]["teams"][i].lowerName == newMyTeams[j]){
+                break;
+            }
+        }
+        if(j == newMyTeams.length){
+            myTeams["myTeams"]["teams"].splice(i,1);
+        }
+        else i++;
+    }}}
+    for(i = 0; i < newMyTeams.length; i++ ){
+        var j = 0;
+        for(j = 0; j < myTeams["myTeams"]["teams"].length; j++){
+            if(myTeams["myTeams"]["teams"][j].lowerName == newMyTeams[i]){
+                break;
+            }
+        }
+
+        if(j == myTeams["myTeams"]["teams"].length){
+            if(allTeams["divisions"]["north"][newMyTeams[i]]){
+                var team = allTeams["divisions"]["north"][newMyTeams[i]];
+                pushObj = {
+                    lowerName: team.lowerName,
+                    name: team.name,
+                    mascot:team.mascot,
+                    logoURL:team.logoURL,
+                    record: team.record,
+                    confWins: team.confWins,
+                    teamColor: team.teamColor
+                }
+                myTeams["myTeams"]["teams"].push(pushObj);
+            }
+            else if(allTeams["divisions"]["south"][newMyTeams[i]]){
+                var team = allTeams["divisions"]["south"][newMyTeams[i]];
+                pushObj = {
+                    lowerName: team.lowerName,
+                    name: team.name,
+                    mascot:team.mascot,
+                    logoURL:team.logoURL,
+                    record: team.record,
+                    confWins: team.confWins,
+                    teamColor: team.teamColor
+                }
+                myTeams["myTeams"]["teams"].push(pushObj);
+            }
+        }
+    }
+    res.status(200).send("Success");
+});
+
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+  });
+
+/*
+    "name": "",
+    "mascot":"",
+    "logoURL":"",
+    "record": "",
+    "confWins": ,
+    "teamColor": "" 
+*/
